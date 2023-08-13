74a0f2d3c932bbff19e32f499953db48956cc0ba
Debobrata Pal <dev.debobratapal@gmail.com>
2023-08-13 23:54:07 +0530

    Time: 5 ms (91.46%), Space: 42.5 MB (91.72%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,19 +1,15 @@
 class Solution {
     public boolean isPalindrome(int x) {
-
         if (x < 0 || (x % 10 == 0 && x != 0)) {
             return false;
         }
-
         int reversed = 0;
         int original = x;
-
         while (x > 0) {
             int digit = x % 10;
             x /= 10;
             reversed = reversed * 10 + digit;
         }
-
         return original == reversed;
     }
 }

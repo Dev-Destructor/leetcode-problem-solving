3a78f81edbdc0c1fb66d5ea73277f3c37a1b0ece
Debobrata Pal <dev.debobratapal@gmail.com>
2024-09-06 16:30:45 +0530

    Time: 0 ms (100.00%), Space: 45.6 MB (8.23%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -3,10 +3,10 @@ class Solution {
         int n = nums.length;
         int sum = 0;
         
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++) {
             sum += nums[i];
-        }
-        
+        } 
+    
         return ((n * (n + 1)) / 2) - sum;
     }
 }

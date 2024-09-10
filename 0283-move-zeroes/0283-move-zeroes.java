3e32f1405fe403c06c6774ffe9ebc0211601461d
Debobrata Pal <dev.debobratapal@gmail.com>
2024-09-10 23:36:11 +0530

    Time: 2 ms (83.54%), Space: 45.7 MB (72.86%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,27 +1,14 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int n = nums.length - 1;
-        int j = -1;
-        
-        for (int k = 0; k <= n; k++) {
-            if (nums[k] == 0) {
-                j = k;
-                break;
+        int left = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] != 0) {
+                int temp = nums[right];
+                nums[right] = nums[left];
+                nums[left] = temp;
+                left++;
             }
-        }
-        
-        if (j == -1) {
-            return;
-        }
-        
-        for(int i = j + 1; i <= n; i++) {
-            if (nums[i] != 0) {
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-                j++;
-            }
-            
-        }
+        }        
     }
 }

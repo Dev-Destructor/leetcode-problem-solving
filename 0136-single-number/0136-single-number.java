7cb0fd73b157edf54c8247d99083f732abac400a
Debobrata Pal <dev.debobratapal@gmail.com>
2024-09-21 13:46:38 +0530

    Time: 1 ms (99.81%), Space: 45.7 MB (77.70%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,11 +1,11 @@
-import java.util.*;
-
 class Solution {
     public int singleNumber(int[] nums) {
-        int result = 0;
-        for (int num : nums) {
-            result ^= num;
+        int ans = 0; 
+        
+        for(int i = 0; i < nums.length; i++) {
+            ans = ans ^ nums[i];
         }
-        return result;
+        
+        return ans;
     }
 }

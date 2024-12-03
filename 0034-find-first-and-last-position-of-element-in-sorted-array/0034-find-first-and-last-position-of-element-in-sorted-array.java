4a24a19742b70b696b7abeaeea16bc20c4da460f
Debobrata Pal <dev.debobratapal@gmail.com>
2024-12-03 22:36:01 +0530

    Time: 0 ms (100.00%), Space: 46 MB (36.16%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,25 +1,18 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         int n = nums.length;
-        int l = 0, h = n - 1;
+        return new int[] {findFirst(nums, n, target), findLast(nums, n, target)};
+    }
+    
+    public int findFirst(int[] nums, int n, int target) {
+        int l = 0, h = n - 1, res = -1;
+        
         
         while(l <= h) {
             int m = (l + h) / 2;
-            
             if(nums[m] == target) {
-                int fo = m, lo = m, cm = m;
-                while(m != 0 && nums[m - 1] == nums[m]) {
-                    m--;
-                }
-                fo = m;
-                
-                while(cm != n - 1 && nums[cm] == nums[cm + 1]) {
-                    cm++;
-                }
-                lo = cm;
-                
-                return new int[] {fo, lo};
-                
+                res = m;
+                h = m - 1;
             } else if(nums[m] > target) {
                 h = m - 1;
             } else {
@@ -27,6 +20,25 @@ class Solution {
             }
         }
         
-        return new int[] {-1, -1};
+        return res;
+    }
+    
+    public int findLast(int[] nums, int n, int target) {
+        int l = 0, h = n - 1, res = -1;
+        
+        
+        while(l <= h) {
+            int m = (l + h) / 2;
+            if(nums[m] == target) {
+                res = m;
+                l = m + 1;
+            } else if(nums[m] > target) {
+                h = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+        
+        return res;
     }
 }

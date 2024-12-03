928443a7717a3efb2d4aca8064c8c1c3df9d581f
Debobrata Pal <dev.debobratapal@gmail.com>
2024-12-03 22:04:10 +0530

    Time: 0 ms (100.00%), Space: 46 MB (36.16%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,53 +1,32 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int fI = firstOccurence(nums, target);
-        int lI = lastOccurence(nums, target);
+        int n = nums.length;
+        int l = 0, h = n - 1;
         
-        return new int[]{fI, lI};
-    }
-    
-    public int firstOccurence(int[] nums, int target) {
-        int low = 0;
-        int high = nums.length - 1;
-        int fO = -1;
-        
-        while(low <= high) {
-            int mid = (low + high) / 2;
+        while(l <= h) {
+            int m = (l + h) / 2;
             
-            if(nums[mid] == target) {
-                fO = mid;
-                high = mid - 1;
-            }
-            else if(nums[mid] > target) {
-                high = mid - 1;
-            }
-            else {
-                low = mid + 1;
+            if(nums[m] == target) {
+                int fo = m, lo = m, cm = m;
+                while(m != 0 && nums[m - 1] == nums[m]) {
+                    m--;
+                }
+                fo = m;
+                
+                while(cm != n - 1 && nums[cm] == nums[cm + 1]) {
+                    cm++;
+                }
+                lo = cm;
+                
+                return new int[] {fo, lo};
+                
+            } else if(nums[m] > target) {
+                h = m - 1;
+            } else {
+                l = m + 1;
             }
         }
         
-        return fO;
-    }
-    
-    public int lastOccurence(int[] nums, int target) {
-        int low = 0;
-        int high = nums.length - 1;
-        int lO = -1;
-        
-        while(low <= high) {
-            int mid = (low + high) / 2;
-            
-            if(nums[mid] == target) {
-                lO = mid;
-                low = mid + 1;
-            }
-            else if(nums[mid] > target) {
-                high = mid - 1;
-            }
-            else {
-                low = mid + 1;
-            }
-        }
-        return lO;
+        return new int[] {-1, -1};
     }
 }
